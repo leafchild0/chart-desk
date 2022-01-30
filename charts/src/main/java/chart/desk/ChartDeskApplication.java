beb6ba89831c9aaa600d6f4e927099195473ed4b
Artem Ishchenko <artem.ischchenko@gmail.com>
2022-01-30 18:11:29 +0200

    init

---
+++ charts/src/main/java/chart/desk/ChartDeskApplication.java
@@ -0,0 +1,11 @@
+package chart.desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartDeskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChartDeskApplication.class, args);
+    }
+}
