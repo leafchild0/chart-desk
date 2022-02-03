f0adfd68dc84d65e581ad1ff9434055db878f69e
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-03 13:13:13 +0200

    Security layer

---
+++ gateway/src/main/java/com/chartdesk/gateway/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.chartdesk.gateway;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.chartdesk.gateway.security.JwtTokenAuthenticationFilter;
+import com.chartdesk.gateway.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Security config for the gateway
+ *
+ * @author vmalyshev
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable()
+                .logout().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .anonymous()
+                .and()
+                // Decided to not add error-handling here, but instead each service will do it
+                .exceptionHandling().authenticationEntryPoint(
+                        (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilterAfter(new JwtTokenAuthenticationFilter(tokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                // Allow login and register
+                .antMatchers("/auth/login", "/auth/register").permitAll()
+                // Allow health-check
+                .antMatchers("/health-check").permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
+
---
+++ gateway/src/main/java/com/chartdesk/gateway/security/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.chartdesk.gateway.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Filter that initiates token validation
+ *
+ * @author vmalyshev
+ */
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
+    public static final String PREFIX = "Bearer ";
+    private final JwtTokenProvider tokenProvider;
+
+    public JwtTokenAuthenticationFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse rsp, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = getJwtFromRequest(req);
+        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+            try {
+
+                Claims claims = tokenProvider.getClaims(token);
+                Long userId = tokenProvider.getUserIdFromJWT(claims);
+
+                @SuppressWarnings("unchecked")
+                List<String> authorities = claims.get("authorities", List.class);
+
+                if (userId != null) {
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userId, null,
+                            authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            } catch (Exception ignore) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(req, rsp);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
