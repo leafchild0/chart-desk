3bc69368cf5ce52cebdbae86dc6c836d68cc0ba2
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-03 12:39:32 +0200

    Added main file for gateway

---
+++ gateway/src/main/java/com/chartdesk/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.chartdesk.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author vmalyshev
+ * @date 03.02.2022
+ */
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+}
