1ba6fe4b28173b467e210f8a18ffcd879a035653
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-18 15:11:49 +0200

    DTOs

---
+++ auth/src/main/java/com/chartdesk/auth/dto/LoginDTO.java
@@ -0,0 +1,41 @@
+package com.chartdesk.auth.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * DTO for login
+ *
+ * @author vmalyshev
+ *
+ */
+@Data
+public class LoginDTO
+{
+	@NotBlank
+	private String username;
+
+	@NotBlank
+	private String password;
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+}
---
+++ auth/src/main/java/com/chartdesk/auth/dto/SignUpDTO.java
@@ -0,0 +1,56 @@
+package com.chartdesk.auth.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * DTO for sign up
+ *
+ * @date 18.02.2022
+ * @author vmalyshev
+ */
+@Data
+public class SignUpDTO
+{
+	@NotBlank
+	private String username;
+
+	@NotBlank
+	@Email
+	private String email;
+
+	@NotBlank
+	private String password;
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+}
