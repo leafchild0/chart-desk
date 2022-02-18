3dc1d683a75dd963c3151a7addad05df2ab1e7bf
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-18 11:38:37 +0200

    Properties change, added yaml, cleanup

--- gateway/src/main/java/com/chartdesk/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/chartdesk/gateway/GatewayApplication.java
@@ -1,8 +1,8 @@
 package com.chartdesk.gateway;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author vmalyshev
  * @date 03.02.2022
  */
-@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
