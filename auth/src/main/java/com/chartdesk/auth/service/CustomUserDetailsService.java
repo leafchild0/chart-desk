fd3b414e5a888a61490877863e4d61c7d6340984
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-22 15:36:21 +0200

    error if user already exists

--- auth/src/main/java/com/chartdesk/auth/service/CustomUserDetailsService.java
+++ auth/src/main/java/com/chartdesk/auth/service/CustomUserDetailsService.java
@@ -4,10 +4,12 @@ import com.chartdesk.auth.dto.SignUpDTO;
 import com.chartdesk.auth.model.User;
 import com.chartdesk.auth.repository.UserRepository;
 import com.chartdesk.auth.security.UserPrincipal;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import reactor.core.publisher.Mono;
 
 /**
@@ -28,13 +30,16 @@ public class CustomUserDetailsService implements ReactiveUserDetailsService
 		this.passwordEncoder = passwordEncoder;
 	}
 
-	public boolean existsByUsername(String username)
+	private boolean existsByUsername(String username)
 	{
 		return userRepository.findByUsername(username).isPresent();
 	}
 
 	public Mono<User> createNewUser(SignUpDTO registerDTO)
 	{
+		if (existsByUsername(registerDTO.getUsername())) {
+			throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY, "User with this username already exists");
+		}
 		User user = new User(registerDTO.getUsername(),
 			registerDTO.getEmail(), passwordEncoder.encode(registerDTO.getPassword()));
 		return Mono.just(userRepository.save(user));
