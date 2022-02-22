06e717be6095a27927ad2ae04798eb6e6c4cd152
viktor.malyshev <viktor.malyshev-cs@insticator.com>
2022-02-22 14:21:45 +0200

    enable webflux

--- auth/src/main/java/com/chartdesk/auth/AuthServiceApplication.java
+++ auth/src/main/java/com/chartdesk/auth/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package com.chartdesk.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author vmalyshev
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFlux
 public class AuthServiceApplication {
 
     public static void main(String[] args)
