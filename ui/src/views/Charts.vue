a57fe6a4421f56260ab8bb5378b5d9f784d5e04b
viktor.malyshev <vmalyshev0@gmail.com>
2022-03-09 17:42:12 +0200

    views stubs

---
+++ ui/src/views/Charts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="charts">
+    <p>This is Charts</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Charts',
+  components: {
+  }
+}
+</script>
---
+++ ui/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <p>This is Home</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
