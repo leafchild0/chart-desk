ce8960454eb4a14dbb4cc8bee7f77df272bbf44c
viktor.malyshev <vmalyshev0@gmail.com>
2022-03-09 17:42:48 +0200

    main component and file

---
+++ ui/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <section>
+      <b-breadcrumb align="is-centered">
+        <b-breadcrumb-item href="/">Home</b-breadcrumb-item>
+        <b-breadcrumb-item href="/charts">Charts</b-breadcrumb-item>
+        <b-breadcrumb-item href="/about">About</b-breadcrumb-item>
+      </b-breadcrumb>
+    </section>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
