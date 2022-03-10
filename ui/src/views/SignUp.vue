5ec9fa94c6866e484f3946393037afd590d63bee
viktor.malyshev <vmalyshev0@gmail.com>
2022-03-10 15:57:25 +0200

    form and validation

--- ui/src/views/SignUp.vue
+++ ui/src/views/SignUp.vue
@@ -1,82 +1,77 @@
 <template>
 	<div class='signup'>
-		<v-container class='fill-height' fluid>
-			<v-row align='center' justify='center'>
-				<v-col cols='12' sm='8' md='4'>
-					<v-card class='elevation-12'>
-						<v-toolbar color='secondary' dark flat>
-							<v-toolbar-title class='headline mb-1'>Sign up</v-toolbar-title>
-						</v-toolbar>
-						<v-card-text>
-							<v-form v-model='valid' ref='form'>
-								<v-text-field
-										label='Username'
-										name='username'
-										type='text'
-										:rules='usernameRules'
-										counter
-										hint='More than 6 characters, letters and numbers'
-										required
-										v-model='username'/>
+		<section class='section'>
+			<div class='container'>
+				<div class='modal-card'>
+					<header class='modal-card-head'>
+						<p class='modal-card-title'>Sign Up</p>
+					</header>
+					<section class='modal-card-body'>
+						<b-field label='Username'>
+							<b-input
+								type='text'
+								v-model='username'
+								placeholder='Username'
+								maxlength='15'
+								validation-message='Only lowercase letter and numbers are allowed'
+								pattern='[a-z0-9]*'
+								required>
+							</b-input>
+						</b-field>
 
-								<v-text-field
-										label='First name'
-										name='firstname'
-										type='text'
-										required
-										:rules='[v => !!v || "First name is required"]'
-										v-model='firstName'/>
+						<b-field label='First Name'>
+							<b-input
+								type='text'
+								v-model='firstName'
+								placeholder='First Name'
+								maxlength='30'
+								required>
+							</b-input>
+						</b-field>
 
-								<v-text-field
-										label='Last name'
-										name='lastname'
-										type='text'
-										required
-										:rules='[v => !!v || "Last name is required"]'
-										v-model='lastName'/>
+						<b-field label='Last Name'>
+							<b-input
+								type='text'
+								v-model='lastName'
+								placeholder='Last Name'
+								maxlength='30'
+								required>
+							</b-input>
+						</b-field>
 
-								<v-text-field
-										label='Password'
-										name='password'
-										type='password'
-										:rules='passwordRules'
-										counter
-										hint='Provide password'
-										required
-										v-model='password'/>
-
-								<v-text-field
-										id='password'
-										label='Confirm password'
-										name='confirm_password'
-										type='password'
-										:rules='[v => v === this.password || "Passwords are not equal"]'
-										hint='Confirm password'
-										required/>
-							</v-form>
-						</v-card-text>
-						<v-card-actions>
-							<v-btn text color='green darken-2' @click='goToLogin'>Have account? Login</v-btn>
-							<v-spacer/>
-							<v-tooltip bottom>
-								<template v-slot:activator='{ on }'>
-									<v-btn
-											text
-											icon
-											color='red lighten-2'
-											v-on='on'
-											@click='clearForm'>
-										<v-icon>mdi-delete</v-icon>
-									</v-btn>
-								</template>
-								<span>Clear the form</span>
-							</v-tooltip>
-							<v-btn @click='signUp' :class='{ grey: !valid, green: valid }'>Sign Up</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-col>
-			</v-row>
-		</v-container>
+						<b-field label='Password'>
+							<b-input
+								type='password'
+								v-model='password'
+								password-reveal
+								min='8'
+								validation-message='Min 6 characters, at least one lower case and one upper case'
+								pattern='(?=.*[0-9])(?=.*[a-zA-Z])(?=\S+$).{6,20}'
+								placeholder='Your password'
+								required>
+							</b-input>
+						</b-field>
+						<b-field label='Confirm Password'>
+							<b-input
+								type='password'
+								v-model='confirmPassword'
+								password-reveal
+								placeholder='Confirm password'
+								required>
+							</b-input>
+						</b-field>
+					</section>
+					<footer class='modal-card-foot'>
+						<b-button
+							label='Sign Up'
+							type='is-info' @click='signUp' :disabled='!isValid'/>
+						<b-button type='is-text' @click='goToLogin'>
+							Have account? Login
+						</b-button>
+					</footer>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
 
@@ -84,53 +79,42 @@
 	import authApi from '../auth/authApi';
 
 	export default {
-		name: 'signup',
-		data: function() {
+		name: 'Signup',
+		data: function () {
 			return {
-				image: null,
 				password: '',
 				username: '',
 				firstName: '',
 				lastName: '',
-				valid: false,
-				passwordRules: [
-					v => !!v || 'Password is required',
-					v => (v && v.length >= 6) || 'Name must be more than 6 characters',
-					v =>
-						/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(v) ||
-						'At least one uppercase, one lowercase, one digit required',
-					v =>
-						(v && v.length < 14) || 'Username should be less than 14 characters',
-					v => v === this.password || 'Password are not equal'
-				],
-				usernameRules: [
-					v => !!v || 'Username is required',
-					v => (v && v.length >= 6) || 'Username must be more than 6 characters',
-					v => /^[a-z0-9]+$/.test(v) || 'Only lowercase letters and numbers allowed',
-					v =>
-						(v && v.length < 14) || 'Username should be less than 14 characters'
-				]
-			};
+				confirmPassword: '',
+			}
+		},
+		computed: {
+			isValid() {
+				return this.username !== ''
+					&& this.password !== ''
+					&& this.firstName !== ''
+					&& this.lastName !== ''
+					&& this.confirmPassword !== ''
+			}
 		},
 		methods: {
-			signUp: function() {
-				this.validateForm();
-
-				if (this.valid) {
+			signUp: function () {
+				if (this.isValid) {
 					authApi
 						.post('auth/signup', {
 							username: this.username,
 							password: this.password,
-							name: this.firstName,
-							surname: this.lastName,
+							firstName: this.firstName,
+							lastName: this.lastName,
 						})
 						.then(
 							() => {
 								this.$router.replace('login');
 							},
 							(error) => {
-								if(error.response.status === 400
-									&& error.response.data.message === 'Username is already in use'){
+								if (error.response.status === 400
+									&& error.response.data.message === 'Username is already in use') {
 									this.$toastr.e('Username is already in use');
 								} else {
 									this.$toastr.e('Ups... Something went wrong');
@@ -141,26 +125,15 @@
 			},
 			goToLogin() {
 				this.$router.replace('login');
-			},
-			validateForm() {
-				this.$refs.form.validate();
-			},
-			clearForm() {
-				this.$refs.form.reset();
 			}
 		}
 	};
 </script>
 
 <style scoped lang="scss">
-	.signup {
-		display: block;
-		height: 100%;
-		width: 100%;
-	}
-
-	.user-image {
-		max-height: 100%;
-		max-width: 100%;
-	}
+.signup {
+	display: block;
+	height: 100%;
+	width: 100%;
+}
 </style>
