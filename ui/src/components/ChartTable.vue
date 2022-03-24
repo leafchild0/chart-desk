94fcba407e4b4269d7e3659b1b12ba9a738bb9e2
Artem Ishchenko <artem.ischchenko@gmail.com>
2022-03-24 13:44:14 +0200

    center fields

--- ui/src/components/ChartTable.vue
+++ ui/src/components/ChartTable.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <b-table :data="data">
+    <b-table paginated per-page="1" :data="data">
 
-      <b-table-column field="name" label="Chart name" centered sortable v-slot="props">
+      <b-table-column field="name" label="Chart name" centered sortable searchable v-slot="props">
         {{ props.row.name }}
       </b-table-column>
 
-      <b-table-column field="version" label="Chart version" centered v-slot="props">
+      <b-table-column field="version" label="Chart version" centered searchable v-slot="props">
         {{ props.row.version }}
       </b-table-column>
 
-      <b-table-column field="description" label="Description" centered sortable v-slot="props">
+      <b-table-column field="description" label="Description" centered searchable v-slot="props">
         {{ props.row.description }}
       </b-table-column>
 
-      <b-table-column class="has-text-success" field="created" label="Date" centered sortable v-slot="props">
+      <b-table-column class="has-text-success" field="created" label="Date" centered searchable sortable v-slot="props">
                 <span class="tag is-success">
                     {{ new Date(props.row.created).toLocaleString() }}
                 </span>
@@ -51,3 +51,16 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.b-table div.top.level {
+  justify-content: center;
+}
+
+.b-table .table th .th-wrap {
+  justify-content: center;
+  display: flex;
+}
+
+</style>
