39b68ee68924bf592e13873d1f59bbcdb7cc0a3c
leafchild0 <vmalyshev0@gmail.com>
2022-03-25 13:11:20 +0200

    Added missing routes

--- ui/routes.js
+++ ui/routes.js
@@ -8,6 +8,13 @@ const express = require('express');
 const router = express.Router();
 const dummyToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijox' +
 	'NTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const user = {
+	firstName: 'John',
+	lastName: 'Doe',
+	email: 'johndoe@email.com',
+	username: 'jonnydoe',
+	isAdmin: false
+}
 
 router
 	.post('/auth/login', (req, res, next) => {
@@ -18,6 +25,21 @@ router
 	.post('/auth/signup', (req, res, next) => {
 		console.log('Sign Up with following data', req.body);
 		res.sendStatus(201);
+		next();
+	})
+	.get('/auth/user', (req, res, next) => {
+		console.log('Get current user');
+		res.json(user);
+		next();
+	})
+	.put('/auth/user', (req, res, next) => {
+		console.log('Update current user', req.body);
+		res.sendStatus(200);
+		next();
+	})
+	.post('/auth/password', (req, res, next) => {
+		console.log('Update current user password', req.body);
+		res.sendStatus(200);
 		next();
 	})
 
