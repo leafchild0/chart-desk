1efa4fa9b6f1be9b259eb6bd52c3e4a2a105cabc
Artem Ishchenko <artem.ischchenko@gmail.com>
2022-03-29 18:03:48 +0300

    fix es lint

--- ui/src/components/ChartTable.vue
+++ ui/src/components/ChartTable.vue
@@ -1,27 +1,27 @@
 <template>
-  <div>
-    <b-table paginated per-page='30' :data='data'>
+	<div>
+		<b-table paginated per-page='30' :data='data'>
 
-      <b-table-column field='name' label='Chart name' centered sortable searchable v-slot='props'>
-        {{ props.row.name }}
-      </b-table-column>
+			<b-table-column field='name' label='Chart name' centered sortable searchable v-slot='props'>
+				{{ props.row.name }}
+			</b-table-column>
 
-      <b-table-column field='version' label='Chart version' centered searchable v-slot='props'>
-        {{ props.row.version }}
-      </b-table-column>
+			<b-table-column field='version' label='Chart version' centered searchable v-slot='props'>
+				{{ props.row.version }}
+			</b-table-column>
 
-      <b-table-column field='description' label='Description' centered searchable v-slot='props'>
-        {{ props.row.description }}
-      </b-table-column>
+			<b-table-column field='description' label='Description' centered searchable v-slot='props'>
+				{{ props.row.description }}
+			</b-table-column>
 
-      <b-table-column class='has-text-success' field='created' label='Date' centered searchable sortable v-slot='props'>
+			<b-table-column class='has-text-success' field='created' label='Date' centered searchable sortable v-slot='props'>
                 <span class='tag is-success'>
                     {{ new Date(props.row.created).toLocaleString() }}
                 </span>
 
-      </b-table-column>
-    </b-table>
-  </div>
+			</b-table-column>
+		</b-table>
+	</div>
 </template>
 
 <script>
@@ -56,13 +56,13 @@
 
 <style>
 
-.b-table div.top.level {
-  justify-content: center;
-}
+	.b-table div.top.level {
+		justify-content: center;
+	}
 
-.b-table .table th .th-wrap {
-  justify-content: center;
-  display: flex;
-}
+	.b-table .table th .th-wrap {
+		justify-content: center;
+		display: flex;
+	}
 
 </style>
