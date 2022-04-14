b995c54f68db8054fd27050676591d7f78237642
Artem Ishchenko <artem.ischchenko@gmail.com>
2022-04-14 17:28:58 +0300

    update charts api (#53)
    1. update API that returns all charts per user.
    2. Add API that returns chart details per name/version
    https://trello.com/c/xTj9AQV6/5

--- charts/src/main/java/chart/desk/controllers/ChartController.java
+++ charts/src/main/java/chart/desk/controllers/ChartController.java
@@ -2,17 +2,14 @@ package chart.desk.controllers;
 
 import chart.desk.model.AssetKind;
 import chart.desk.model.ChartEntry;
-import chart.desk.model.ChartIndex;
+import chart.desk.model.ChartTo;
 import chart.desk.model.db.ChartModel;
 import chart.desk.parsers.HelmAttributeParser;
 import chart.desk.services.ChartService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.ByteStreams;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +19,7 @@ import org.springframework.http.codec.multipart.Part;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,41 +30,29 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/{userId}/charts")
 @Slf4j
 public class ChartController {
 
     private final HelmAttributeParser helmAttributeParser;
     private final ChartService chartService;
-    private final ObjectMapper yamlObjectMapper;
-    private final ObjectMapper jsonObjectMapper;
 
     @Autowired
-    public ChartController(HelmAttributeParser helmAttributeParser,
-            ChartService chartService,
-            @Qualifier("yaml_mapper") ObjectMapper yamlObjectMapper,
-            @Qualifier("json_mapper") ObjectMapper jsonObjectMapper) {
+    public ChartController(HelmAttributeParser helmAttributeParser, ChartService chartService) {
         this.helmAttributeParser = helmAttributeParser;
         this.chartService = chartService;
-        this.yamlObjectMapper = yamlObjectMapper;
-        this.jsonObjectMapper = jsonObjectMapper;
     }
 
-    // TODO: need to fetch it from database later and maybe cache here with caffeine cache etc
-    @GetMapping("{userId}/index.yaml")
-    public String getIndexYaml(@PathVariable("userId") String userId) throws JsonProcessingException {
-        ChartIndex index = chartService.getIndex(userId);
-        return yamlObjectMapper.writeValueAsString(index);
+    @GetMapping
+    public List<ChartTo> getIndexJson(@PathVariable("userId") String userId) {
+        return chartService.getChartList(userId);
     }
 
-    @GetMapping("{userId}/index.json")
-    public String getIndexJson(@PathVariable("userId") String userId) throws JsonProcessingException {
-        ChartIndex index = chartService.getIndex(userId);
-        return jsonObjectMapper.writeValueAsString(index);
-    }
-
-    @PostMapping(value = "/api/{userId}/charts", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<Mono<ChartModel>> uploadChart(@RequestPart("chart") Flux<FilePart> fileParts, @PathVariable("userId") String userId) {
         Mono<ChartModel> attributes = toByteArray(fileParts).map(bytes -> {
             ChartEntry helmAttributes = getHelmAttributes(bytes, AssetKind.HELM_PACKAGE);
@@ -78,24 +64,11 @@ public class ChartController {
                 .body(attributes);
     }
 
-    @GetMapping(value = "/{userId}/{name}-{version}.tgz")
-    public ResponseEntity<Mono<byte[]>> getChart(@PathVariable("name") String name, @PathVariable("version") String version, @PathVariable("userId") String userId) {
-        byte[] chart = chartService.get(name, version, AssetKind.HELM_PACKAGE, userId);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(Mono.just(chart));
-    }
-
-    @PostMapping("{userId}/api/prov")
-    public ResponseEntity<Mono<ChartModel>> uploadProvinance(@RequestPart("chart") Flux<FilePart> fileParts, @PathVariable("userId") String userId) {
-        Mono<ChartModel> attributes = toByteArray(fileParts).map(bytes -> {
-            ChartEntry helmAttributes = getHelmAttributes(bytes, AssetKind.HELM_PROVENANCE);
-            return chartService.save(helmAttributes, bytes, AssetKind.HELM_PROVENANCE, userId, true);
-        });
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(attributes);
+    @GetMapping(value = "/{name}/{version}")
+    public ResponseEntity<Mono<ChartEntry>> getChart(@PathVariable("name") String name, @PathVariable("version") String version, @PathVariable("userId") String userId) {
+        Optional<ChartEntry> chart = chartService.getChart(name, version, userId);
+        return chart.map(entry -> ResponseEntity.status(HttpStatus.OK).body(Mono.just(entry)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(Mono.empty()));
     }
 
     private Mono<byte[]> toByteArray(Flux<FilePart> fileParts) {
