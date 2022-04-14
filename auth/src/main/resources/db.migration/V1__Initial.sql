8037a10f7d90137ccf6f51cb4df50d132d5c1b90
Victor Malyshev <vmalyshev0@gmail.com>
2022-04-14 16:11:02 +0300

    flyway fix

--- auth/src/main/resources/db.migration/V1__Initial.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    id bigint(20) NOT NULL AUTO_INCREMENT primary key,
-    username varchar(50) not null,
-    password varchar not null,
-    email varchar (100) not null,
-    enabled boolean not null,
-    isAdmin boolean not null default false,
-    UNIQUE KEY uk_users_username (username)
-);
