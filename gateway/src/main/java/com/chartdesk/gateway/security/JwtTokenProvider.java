2d6901052f505228bf181bccb8e637a8ad1594a2
Victor Malyshev <vmalyshev0@gmail.com>
2022-04-15 13:44:10 +0300

    Added temp exceptions

--- gateway/src/main/java/com/chartdesk/gateway/security/JwtTokenProvider.java
+++ gateway/src/main/java/com/chartdesk/gateway/security/JwtTokenProvider.java
@@ -7,7 +7,9 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.crypto.SecretKey;
 
@@ -41,16 +43,19 @@ public class JwtTokenProvider {
 		}
 		catch (MalformedJwtException ex) {
 			log.error("Invalid JWT token");
+			throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid JWT token");
 		}
 		catch (ExpiredJwtException ex) {
 			log.error("Expired JWT token");
+			throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY, "Expired JWT token");
 		}
 		catch (UnsupportedJwtException ex) {
 			log.error("Unsupported JWT token");
+			throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY, "Unsupported JWT token");
 		}
 		catch (IllegalArgumentException ex) {
-			log.error("JWT claims string is empty.");
+			log.error("JWT claims string is empty");
+			throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY, "JWT claims string is empty");
 		}
-		return false;
 	}
 }
