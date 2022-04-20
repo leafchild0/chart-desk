15bfd5d36784c10cf3d47ea86a794565c8d5b87d
Victor Malyshev <vmalyshev0@gmail.com>
2022-04-20 20:43:02 +0300

    API small update

--- ui/src/api/chartsApi.js
+++ ui/src/api/chartsApi.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 const chartsApi = axios.create({
-	baseURL: '/charts_api/'
+	baseURL: '/charts_api/api'
 });
 
 export const chartsList = () => {
-	// TODO: user id/name here & gateway
-	return chartsApi.get('api/2/charts')
+	return chartsApi.get('/2/charts')
 }
 
 export const uploadChart = (payload) => {
-	return chartsApi.post('api/2/charts', payload);
+	return chartsApi.post('/2/charts', payload);
 }
