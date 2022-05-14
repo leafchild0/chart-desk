736d66c8f3308f779f686a0efd8325da036cb664
Victor Malyshev <vmalyshev0@gmail.com>
2022-05-14 16:13:49 +0300

    remove cors

--- charts/src/main/java/chart/desk/configuration/WebFluxConfig.java
+++ charts/src/main/java/chart/desk/configuration/WebFluxConfig.java
@@ -3,25 +3,9 @@ package chart.desk.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
 public class WebFluxConfig {
-
-    @Bean
-    public WebFluxConfigurer corsConfigurer() {
-        return new WebFluxConfigurerComposite() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
-    }
 
     @Bean
     public RestTemplate restTemplate() {
